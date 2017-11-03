f36af39c614dc2db8b179c820af3a961d0727565
LavenderIris <pchung@gmail.com>
2017-11-02 17:53:27 -0700

    retroBarcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'Retro Barcode Generator';
+  colors = ['Tomato', 'Violet', 'Red', 'Blue', 'Yellow', 'Cyan', 'Green', 'Orange', 'Purple', 'Fuchsia', 'SlateBlue', 'LightGray'];
+  rand_colors = [];
+
+  fill_colors() {
+    for (let y = 0; y < 10; y++) {
+      const r_c = this.colors[Math.floor(Math.random() * this.colors.length)];
+      this.rand_colors.push(r_c);
+    }
+  }
+
+  ngOnInit() {
+    this.fill_colors();
+  }
 }
